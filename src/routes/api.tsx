1dce92851df9b89434f382035f092a5319cc8d3b
szhangbiao <szhangbiao@gmail.com>
2025-07-26 17:27:34 +0800

    refactor(routes): remove user-related handlers and services
    - Delete unused user and info handlers
    - Remove user service implementation
    - Update API routes to focus on price-related endpoints

--- src/routes/api.tsx
+++ src/routes/api.tsx
@@ -1,12 +1,10 @@
 import { Hono } from 'hono'
-import { getUserHandler } from '../handler/userHandler'
-import { getInfoHandler } from '../handler/infoHandler'
 import { authMiddleware } from '../middleware/auth'
+
 
 const api = new Hono()
 
-// 只对用户相关路由应用认证中间件
-api.get('/info', getInfoHandler) // 公开接口，不需要认证
-api.get('/users/:id', authMiddleware, getUserHandler) // 需要认证
+api.get('/price/request')
+api.get('/price/crawl')
 
 export default api
